9dfd486890dd41e9d0fda702b1a2d2179efe1b9c
Sarah <sarahrudy@gmail.com>
2022-01-25 21:03:23 -0700

    Add some text

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import './App.css'
+import NavBar from '../NavBar/NavBar'
 
 const App = () => {
   return (
     <div className="App">
-
+      <NavBar />
     </div>
   )
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <div className="NavBar">
+      <h1>Hi, I'm Sarah</h1>
+    </div>
+  )
+}
+
+export default NavBar
