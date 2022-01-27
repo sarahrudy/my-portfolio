2b290556edd6754c63d2c7d0d26a507fe61f9c41
Sarah <sarahrudy@gmail.com>
2022-01-26 20:46:35 -0700

    Add resume, gmail, and turing links

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,8 +3,8 @@ import './About.css'
 const About = () => {
   return (
     <div className="About">
-      <h1 className="name">SARAH RUDY</h1>
-      <p className>Here's some information about me.</p>
+      <h1>HI, I'M SARAH RUDY - A SOFTWARE ENGINEER . </h1>
+      <p className>I created this portfolio website as a way to showcase the projects I've been working on. </p>
     </div>
   )
 }
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,6 +1,10 @@
 import './SideBar.css'
-import linkedIn from '../../images/linkedin.png';
-import gitHub from '../../images/github.png';
+import linkedIn from '../../images/linkedin.png'
+import gitHub from '../../images/github.png'
+import gmail from '../../images/gmail.png'
+import resumeLogo from '../../images/resumeLogo.png'
+import resume from '../../images/sarah-rudy-resume.png'
+import turingLogo from '../../images/turing-logo.png'
 
 const SideBar = () => {
   return (
@@ -13,6 +17,21 @@ const SideBar = () => {
       <div>
         <a href="https://github.com/sarahrudy" target="_blank" rel="noopener noreferrer">
           <img className="git-hub" src={ gitHub } alt="Link to GitHub page"/>
+        </a>
+      </div>
+      <div>
+        <a href={'mailto:sarahrudy@gmail.com'} target="_blank" rel="noopener noreferrer">
+          <img className="email" src={ gmail } alt="Link to email" />
+        </a>
+      </div>
+      <div>
+        <a href={ resume } target="_blank" rel="noopener noreferrer">
+          <img className="resume" src={ resumeLogo } alt="Link to resume" />
+        </a>
+      </div>
+      <div>
+        <a href="https://turing.edu/" target="_blank" rel="noopener noreferrer">
+          <img className="turing" src={ turingLogo } alt="Link to Turing website" />
         </a>
       </div>
     </div>
